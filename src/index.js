2e9292b36a5d88b9e18e3dc3e48ccaba0e60659b
Akylbekov <user@Users-MacBook-Pro.local>
2023-01-09 01:48:18 +0600

    воспроизведено 156. Вставка элементов через props.children

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styled from 'styled-components';
+import {Button} from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const BigButton = styled(Button)`
+    margin: 0 auto;
+    width: 245px;
+    text-align: center;
+`;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App/>
+        <BigButton as="a">Отправить отчет</BigButton>
+    </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
